70446810914051b41b307e001ec39cfaf5b7f9a6
Khi Kidman <khi.kidman@gmail.com>
2025-07-03 20:48:43 -0500

    Habit progress bars added, needs backend support

---
+++ Productivity/Concept Testing/NewHabitCardView.swift
@@ -0,0 +1,18 @@
+//
+//  NewHabitCardView.swift
+//  Productivity
+//
+//  Created by Khi Kidman on 7/3/25.
+//
+
+import SwiftUI
+
+struct NewHabitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewHabitCardView()
+}
